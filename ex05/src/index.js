c846c2add2207c4154e193f90a46c0da60cc7500
VuduPopaj <adnanduljkovic@hotmail.com>
2021-03-18 23:23:31 +0100

    Added ex05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,5 +1,12 @@
 // Only change code below this line
-
+var myNestedArray = [
+  ["Toblerone", 5],
+  ["Milka", 3],
+];
+function myNestedFunction(arr) {
+  var arr = myNestedArray;
+  return arr;
+}
 // Only change code above this line
 console.log(myNestedFunction()); // Change this line
 module.exports = myNestedFunction;
